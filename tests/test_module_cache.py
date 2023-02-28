25c0bc023fc73bda2323c5d622abefe25cbc0d4c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-28 13:35:23 -0500

    Update test_module_cache.py

--- tests/test_module_cache.py
+++ tests/test_module_cache.py
@@ -7,7 +7,7 @@ def test_doc_cache():
     cache = ModuleCache()
     cache.module_cookie = 0xB81C
     cache.project_cookie = 0x08F3
-    cache.misc = [0x0316, "x", 0x032D, 0x0123, 0x88, 8, 0x18, "00000000", 1]
+    cache.misc = [0x0316, 0x032D, 0x0123, 0x88, 8, 0x18, "00000000", 1]
     guid = uuid.UUID('0002081900000000C000000000000046')
     cache.guid = bytes(("0{" + str(guid) + "}").upper(), "utf_16_le")
 
@@ -32,7 +32,7 @@ def test_module_cache():
     cache = ModuleCache()
     cache.module_cookie = 0xB241
     cache.project_cookie = 0x08F3
-    cache.misc = [0x0316, "x", 0x027D, 3, 0, 2, 0xFFFF, "FFFFFFFF", 0]
+    cache.misc = [0x0316, 0x027D, 3, 0, 2, 0xFFFF, "FFFFFFFF", 0]
     cache.indirect_table = struct.pack("<iI", -1, 0x78)
     f = open('tests/vbaProject.bin', 'rb')
     f.seek(0x1200)
