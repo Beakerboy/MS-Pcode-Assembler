c4f7547a2a4051ff49bec86ebfa4a3e67d89aa48
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-28 11:57:11 -0500

    Update test_module_cache.py

--- tests/test_module_cache.py
+++ tests/test_module_cache.py
@@ -30,7 +30,7 @@ def test_doc_cache():
 
 def test_module_cache():
     cache = ModuleCache()
-    cache.cookie = 0xB241
+    cache.module_cookie = 0xB241
     cache.project_cookie = 0x08F3
     cache.misc = [0x0316, "x", 0x027D, 3, 0, 2, 0xFFFF, "FFFFFFFF", 0]
     cache.indirect_table = struct.pack("<iI", -1, 0x78)
