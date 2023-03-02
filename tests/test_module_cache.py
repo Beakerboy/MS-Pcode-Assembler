f7c2197a2a24b8ef072ba74b4520ee8bee2bea59
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-02 12:53:32 -0500

    Update test_module_cache.py

--- tests/test_module_cache.py
+++ tests/test_module_cache.py
@@ -35,3 +35,65 @@ def test_module_cache():
     f.seek(0x1200)
     file_data = f.read(0x0283)
     assert cache.to_bytes() == file_data
+
+def test_full_cache():
+    cache = ModuleCache(0xB2, 0x78B9)
+    cache.module_cookie = 0x0399
+    cache.misc = [0x0316, 0x0123, 0x88, 8, 0x18, "00000000", 1]
+    guid = uuid.UUID('fcfb3d2aa0fa1068a73808002b3371b5')
+    cache.guid = [guid]
+
+    indirect_table = ("0C 21 32 02 78 00 00 00 01 00 03 68 00 00 00 00",
+                      "FF FF FF FF FF FF FF FF 00 00 00 00 00 00 00 00",
+                      "00 00 00 00 00 00 00 00 FF FF FF FF 00 00 00 00",
+                      "0A 00 00 00 00 00 00 00 58 00 00 00 0C 00 FF FF",,
+                      "38 00 00 00 05 00 05 00 05 00 00 00 00 00 00 00",
+                      "BC 01 00 03 00 00 00 00 69 83 FE FF FF FF FF FF",
+                      "FF FF FF FF FF FF FF FF 0C 01 FF FF 00 00 00 00",
+                      "FF FF FF FF 20 00 00 00 0C 41 34 02 F0 00 00 00",
+                      "00 00 03 68 00 00 00 00 FF FF FF FF FF FF FF FF",
+                      "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                      "FF FF FF FF 00 00 00 00 0A 00 00 00 00 00 00 00",
+                      "D0 00 00 00 FF FF FF FF 40 00 01 00 05 00 05 00",
+                      "0A 00 00 00 00 00 00 00 94 01 00 03 00 00 00 00",
+                      "69 83 36 02 FF FF FF FF FF FF FF FF FF FF FF FF",
+                      "08 01 FF FF 00 00 00 00 FF FF FF FF 80 01 00 00",
+                      "0C 11 38 02 48 01 00 00 02 00 03 60 00 00 00 00",
+                      "FF FF FF FF FF FF FF FF 00 00 00 00 00 00 00 00",
+                      "00 00 00 00 00 00 00 00 FF FF FF FF 00 00 00 00",
+                      "0A 00 00 00 00 00 00 00 FF FF FF FF FF FF FF FF",
+                      "48 00 02 00 05 00 05 00 0F 00 00 00 00 00 00 00",
+                      "94 00 00 03 00 00 00 00 0C 11 3A 02 A0 01 00 00",
+                      "03 00 03 60 00 00 00 00 FF FF FF FF FF FF FF FF",
+                      "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                      "FF FF FF FF 00 00 00 00 0A 00 00 00 00 00 00 00",
+                      "FF FF FF FF FF FF FF FF 50 00 03 00 05 00 05 00",
+                      "14 00 00 00 00 00 00 00 94 00 00 03 00 00 00 00",
+                      "0C 11 3C 02 FF FF FF FF 04 00 03 60 00 00 00 00",
+                      "FF FF FF FF FF FF FF FF 00 00 00 00 00 00 00 00",
+                      "00 00 00 00 00 00 00 00 FF FF FF FF 00 00 00 00",
+                      "0A 00 00 00 00 00 00 00 F8 01 00 00 0C 00 FF FF",
+                      "58 00 04 00 04 00 04 00 19 00 00 00 00 00 00 00",
+                      "BC 01 00 03 00 00 00 00 69 83 FE FF FF FF FF FF",
+                      "FF FF FF FF FF FF FF FF 0C 01 FF FF 00 00 00 00",
+                      "FF FF FF FF 20 00 00 00 02 80 FE FF FF FF FF FF",
+                      "20 00 00 00 FF FF FF FF 48 02 00 00 02 01 FF FF",
+                      "18 02 00 00 00 00 00 00 FF FF FF FF FF FF FF FF",
+                      "00 00 00 00 00 00 00 00 1D 00 00 00 25 00 00 00",
+                      "FF FF FF FF A0 01 00 00 00 00 00 00 FF FF FF FF",
+                      "FF FF FF FF 78 00 00 00 F0 00 00 00 48 01 00 00",
+                      "02 83 FE FF FF FF FF FF 00 00 00 00 FF FF FF FF",
+                      "A0 02 00 00 00 00 FF FF FF FF FF FF 00 00 00 00",
+                      "FF FF FF FF FF FF FF FF 00 00 00 00 FF FF FF FF",
+                      "1D 00 18 00 25 00 00 00 FF FF FF FF 50 01 00 00",
+                      "FF FF FF FF A0 01 00 00")
+    cache.indirect_table = bytes.fromhex(" ".join(indirect_table))
+    object_table = ("02 00 53 10 FF FF FF FF 00 00 01 00 53 0C FF FF",
+                    "FF FF 00 00 01 00 53 94 FF FF FF FF 00 00 00 00",
+                    "36 22 FF FF FF FF 00 00")
+    cache.object_table = bytes.fromhex(" ".join(object_table))
+
+    f = open('tests/SQL-vbaProject.bin', 'rb')
+    f.seek(0x1200)
+    file_data = f.read(0x08C4)
+    assert cache.to_bytes() == file_data
