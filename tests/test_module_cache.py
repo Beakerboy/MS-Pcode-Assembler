581634d58c1a7675d8df25cdb54482bfabdd10bf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-11-12 14:02:53 -0500

    Update test_module_cache.py

--- tests/test_module_cache.py
+++ tests/test_module_cache.py
@@ -6,7 +6,7 @@ from ms_pcode_assembler.module_cache import ModuleCache
 def test_doc_cache() -> None:
     cache = ModuleCache(0xB5, 0x08F3)
     cache.module_cookie = 0xB81C
-    cache.misc = [0x0316, 0, 0x0123, 0x88, 8, 0x18, "00000000", 1, -1, 7]
+    cache.misc = [0x0316, 0, 0x0123, 0x88, 8, 0x18, "00000000", 1, -1, 0]
     guid = uuid.UUID('0002081900000000C000000000000046')
     cache.guid = [guid]
 
@@ -29,7 +29,7 @@ def test_doc_cache() -> None:
 def test_module_cache() -> None:
     cache = ModuleCache(0xB5, 0x08F3)
     cache.module_cookie = 0xB241
-    cache.misc = [0x0316, 0, 3, 0, 2, 0xFFFF, "FFFFFFFF", 0, -1]
+    cache.misc = [0x0316, 0, 3, 0, 2, 0xFFFF, "FFFFFFFF", 0, -1, 0]
     cache.indirect_table = struct.pack("<iI", -1, 0x78)
     f = open('tests/vbaProject.bin', 'rb')
     f.seek(0x1200)
