f31143691af1b84e3a3afcb09049be93bdfba20a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-11-12 13:14:38 -0500

    Update module_cache.py

--- src/ms_pcode_assembler/module_cache.py
+++ src/ms_pcode_assembler/module_cache.py
@@ -175,7 +175,7 @@ class ModuleCache():
         for rf in self.rfff_data:
             rf_len += len(rf) * 2 + 3
 
-        return self.rfff_offset() + rf_len + 3
+        return self.rfff_offset() + rf_len + 1
 
     def magic_offset(self: T) -> int:
         """
