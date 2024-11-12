0257ecaf87b29e8ddd50ffff8151a47322442b7b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-11-12 13:44:38 -0500

    Update test_module_cache.py

--- tests/test_module_cache.py
+++ tests/test_module_cache.py
@@ -175,8 +175,8 @@ def test_full_cache() -> None:
     file_data = f.read(len(gu))
     assert gu == file_data
     new_sec = (struct.pack("<IihIhIhHIHhIH", 0x454D, -1, -1, 0, -1,
-                          0, -1, 0x0101, 0, 0xDF, -1, 0, misc[5])
-              + b'\xFF' * 0x80)
+                           0, -1, 0x0101, 0, 0xDF, -1, 0, misc[5])
+               + b'\xFF' * 0x80)
     file_data = f.read(len(new_sec))
     assert new_sec == file_data
     file_data = f.read(0x08C4)
