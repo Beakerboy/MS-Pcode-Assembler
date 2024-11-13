94d42d2226505268117d5d726c0f1d71a7d98a41
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-11-13 08:22:41 -0500

    Update module_cache.py

--- src/ms_pcode_assembler/module_cache.py
+++ src/ms_pcode_assembler/module_cache.py
@@ -114,7 +114,7 @@ class ModuleCache():
                 guid_str += "{" + str(guid).upper() + "}"
             guid_str_bytes = bytes(guid_str, "utf_16_le")
             ca += len(guid_str_bytes).to_bytes(2, "little") + guid_str_bytes
-        ca += struct.pack("<IHiH", self.misc[9], 0, -1, 0x0101)
+        ca += struct.pack("<IHiH", self.misc[6], 0, -1, 0x0101)
         return ca
 
     def indirect_table_section(self: T) -> bytes:
