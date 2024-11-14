ddb0aa6850a93f1fa25b62a68c2b0a0578e8fcf3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-11-14 08:49:29 -0500

    Update cache_header.py

--- src/ms_pcode_assembler/cache_header.py
+++ src/ms_pcode_assembler/cache_header.py
@@ -8,7 +8,7 @@ T = TypeVar('T', bound='CacheHeader')
 class CacheHeader():
 
     def __init__(self: T, cache, project_cookie: int,
-                 syskind: int = 2, siganture = 0) -> None:
+                 syskind: int = 2, signature: int = 0) -> None:
         self._cache = cache
         self._project_cookie = project_cookie
         self._signature = signature
