4f62cde26b57f5a1075b5d0d8e4396441feff3f2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-11-14 15:22:21 -0500

    Update test_module_cache.py

--- tests/test_module_cache.py
+++ tests/test_module_cache.py
@@ -18,12 +18,11 @@ def test_doc_cache() -> None:
                       "FF FF FF FF FF FF FF FF 00 00 00 00 2E 00 43 00",
                       "1D 00 00 00 25 00 00 00 FF FF FF FF 40 00 00 00")
     cache.indirect_table = bytes.fromhex(" ".join(indirect_table))
-    
+
     object_table = [[2, 0x4C53], [1, 0x1053], [1, 0x9453], [0, 0x3C02]]
     object_table_bytes = b''
     for entry in object_table:
-        entry.extend([-1, 0])
-        object_table_bytes += struct.pack("<HHiH", *entry)
+        object_table_bytes += struct.pack("<HHiH", *entry, -1, 0)
     cache.object_table = object_table_bytes
 
     f = open('tests/vbaProject.bin', 'rb')
