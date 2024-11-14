59135360a09c1ef3934de588736de6f152fdadea
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-11-14 08:56:52 -0500

    Update cache_header.py

--- src/ms_pcode_assembler/cache_header.py
+++ src/ms_pcode_assembler/cache_header.py
@@ -23,7 +23,8 @@ class CacheHeader():
 
     def to_bytes(self: T) -> bytes:
         cache = self._cache
-        module_cookie = cache.module_cookie
+        mc = cache.module_cookie
+        pc = self._project_cookie
         dfo = cache.df_offset()
         ito = cache.id_table_offset()
         magic_ofs = cache.magic_offset()
@@ -31,8 +32,9 @@ class CacheHeader():
         ffo = cache.four_five_offset()
         edo = cache.end_offset()
         sdo = cache.second_df_offset()
+        d2 = self.data2
         return struct.pack("<BHBIII IiII IIHHHh IIHhH",
                            22, self._signature, self.data1, dfo, rfo, ffo,
                            ito, sdo, magic_ofs, edo,
-                           self.data2, 1, self._project_cookie, module_cookie, 0, -1,
+                           d2, 1, pc, mc, 0, -1,
                            self.data3, self.data4, 0xB6, -1, 0x0101)
