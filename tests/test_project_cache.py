f228a40f701c67921f09960c37b271eac8f484b5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-04 11:08:46 -0500

    Create test_project_cache.py

---
+++ tests/test_project_cache.py
@@ -0,0 +1,14 @@
+import struct
+import uuid
+from ms_pcode_assembler.project_cache import ProjectCache
+
+
+def test_doc_cache() -> None:
+    cache = ProjectCache(0x0404, 0, 0)
+    f = open('tests/SQL-vbaProject.bin', 'rb')
+    f.seek(0x30200)
+    file_data = f.read(1)
+    assert b'\xff' == file_data
+    he = cache._header()
+    file_data = f.read(len(he))
+    assert he == file_data
