c7f694ccfe5fc2c9dbe75fe4d12d3f322441b101
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-04 13:16:26 -0500

    Update test_project_cache.py

--- tests/test_project_cache.py
+++ tests/test_project_cache.py
@@ -36,7 +36,7 @@ def test_doc_cache() -> None:
     #     "*\\G{3AF3885D-1EAB-4BD6-A2CF-8C849267EC75}#2.0#0#" +
     #     "C:\\Users\\KNOWAC~1\\AppData\\Local\\Temp\\VBE\\MSForms.exd#" +
     #     "Microsoft Forms 2.0 Object Library"
-    @ )
+    # )
 
     cache.add_library(
         "*\\G{B691E011-1797-432E-907A-4D8C69339129}#6.1#0#" +
