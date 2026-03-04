0917e2651868574787b0be52a782784e5d8d4e68
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-04 11:11:21 -0500

    Update test_project_cache.py

--- tests/test_project_cache.py
+++ tests/test_project_cache.py
@@ -6,7 +6,7 @@ from ms_pcode_assembler.project_cache import ProjectCache
 def test_doc_cache() -> None:
     cache = ProjectCache(0x0404, 0, 0)
     f = open('tests/SQL-vbaProject.bin', 'rb')
-    f.seek(0x30200)
+    f.seek(0x30207)
     file_data = f.read(1)
     assert b'\xff' == file_data
     he = cache._header()
