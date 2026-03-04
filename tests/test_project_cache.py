c24487fbd2dbc0ae76a9e49c19de6d1f55e3f328
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-04 11:24:50 -0500

    Update test_project_cache.py

--- tests/test_project_cache.py
+++ tests/test_project_cache.py
@@ -6,11 +6,11 @@ from ms_pcode_assembler.project_cache import ProjectCache
 def test_doc_cache() -> None:
     cache = ProjectCache(0x04e4, 0, 0)
     cache.add_library(
-        "*\G{000204EF-0000-0000-C000-000000000046}#4.2#9#" +
-        "C:\Program Files\Common Files\Microsoft Shared\VBA\VBA7.1\VBE7.DLL#" +
+        "*\\G{000204EF-0000-0000-C000-000000000046}#4.2#9#" +
+        "C:\\Program Files\\Common Files\\Microsoft Shared" +
+        "\\VBA\\VBA7.1\\VBE7.DLL#" +
         "Visual Basic For Applications)"
     )
-
     f = open('tests/SQL-vbaProject.bin', 'rb')
     f.seek(0x30207)
     file_data = f.read(1)
