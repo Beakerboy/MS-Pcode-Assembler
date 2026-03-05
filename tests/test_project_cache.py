25704bff2648c63d8dfe8181309ce77dc0912de0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-05 13:02:22 -0500

    Update test_project_cache.py

--- tests/test_project_cache.py
+++ tests/test_project_cache.py
@@ -54,15 +54,27 @@ def test_doc_cache() -> None:
     cache._hex = hex2
     cache._modules = [
         ("ThisWorkbook", 48, 91, hex1, 0x22b, 0xc472, 0, 0x33b),
-        ("Sheet1", 48, 93, hex1, 0x22b, 0xc472, 0, 0x33b),
+        ("Sheet1", 48, 93, hex1, 0x22f, 0xc472, 0, 0x33b),
         ("iSQLConnection", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
         ("iSQLQuery", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
         ("iSQLRecordset", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
         ("SQLCondition", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
         ("SQLConnection", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
         ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
-        (), (), (),
-        (), (), (), (), (), (), (), (), (), ()
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b,
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b),
+        ("SQLCreate", 50, 51, hex2, 0x22b, 0xc472, 0, 0x33b)
     ]
     f = open('tests/SQL-vbaProject.bin', 'rb')
     f.seek(0x30207)
