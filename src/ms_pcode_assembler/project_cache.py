9e1bf0a0ca32f6e2bf581661ace20c259d958e06
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-08 17:26:25 -0400

    Update project_cache.py

--- src/ms_pcode_assembler/project_cache.py
+++ src/ms_pcode_assembler/project_cache.py
@@ -146,7 +146,11 @@ class ProjectCache():
         bin_array = self._post_data
 
         for byte_string in bin_array:
-            record += byte_string + neg_one_one
+            if isinstance(byte_string, tuple):
+                record += (byte_string[0] + byte_string[1] +
+                           struct.pack("<I", 1))
+            else:
+                record += byte_string + neg_one_one
 
         record += neg_one_4b + struct.pack("<I", 0x30)
         ca += struct.pack("<I", len(record)) + record
